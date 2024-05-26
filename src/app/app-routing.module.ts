dde01d1ee1b5a1d022f5e8da93e55ac022ccc82d
ravi <ravisathe004@gmail.com>
2024-05-26 20:30:43 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './user/component/landing-page/landing-page.component';
+import { DashboardComponent } from './user/component/dashboard/dashboard.component';
+import { HomeComponent } from './user/component/home/home.component';
+import { LoginRegistrationComponent } from './user/auth/login-registration/login-registration.component';
 
 const routes: Routes = [
+  {path:'', component:LandingPageComponent, title:'GoFit'},
+  {path:'login-register',component:LoginRegistrationComponent,title:'Login Registration'},
+  {path:'dashboard',component:DashboardComponent,title:'Dashboard'},
+  {path:'home',component:HomeComponent,title:'home'},
   { path: '', loadChildren: () => import('./trainer/trainer.module').then(m => m.TrainerModule) }, 
   { path: 'gym', loadChildren: () => import('./gym/gym.module').then(m => m.GymModule) },
  ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginRegistrationComponent } from './user/auth/login-registration/login-registration.component';
+import { LandingPageComponent } from './user/component/landing-page/landing-page.component';
+import { DashboardComponent } from './user/component/dashboard/dashboard.component';
+import { AboutComponent } from './user/component/about/about.component';
+import { ServiceComponent } from './user/component/service/service.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HomeComponent } from './user/component/home/home.component';
+import { GymsComponent } from './user/tabs/gyms/gyms.component';
+import { TrainerComponent } from './user/tabs/trainer/trainer.component';
+import { GymPreviewComponent } from './user/view-page/gym-preview/gym-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegistrationComponent,
+    LandingPageComponent,
+    DashboardComponent,
+    AboutComponent,
+    ServiceComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    GymsComponent,
+    TrainerComponent,
+    GymPreviewComponent
   ],
   imports: [
     BrowserModule,
